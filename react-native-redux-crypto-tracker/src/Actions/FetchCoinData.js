d40ef66cf492d2ec95b555114c5f27580466c431
Kamal Kumar <kumar@secondchild.nyc>
2019-11-15 16:39:52 -0500

    fixed state issue

--- react-native-redux-crypto-tracker/src/Actions/FetchCoinData.js
+++ react-native-redux-crypto-tracker/src/Actions/FetchCoinData.js
@@ -6,8 +6,10 @@ import {
   FETCHING_COIN_DATA_FAIL
 } from './../Utils/ActionTypes';
 
+
 export default function FetchCoinData() {
   return dispatch => {
+
     dispatch({ type: FETCHING_COIN_DATA })
 
     return axios.get(`${apiBaseURL}/v1/ticker/?limit=10`)
--- react-native-redux-crypto-tracker/src/Reducers/CryptoReducer.js
+++ react-native-redux-crypto-tracker/src/Reducers/CryptoReducer.js
@@ -4,42 +4,43 @@ import {
   FETCHING_COIN_DATA_FAIL
 } from './../Utils/ActionTypes';
 
-const initialState ={
-  isFetching: null,
+const initialState = {
+  isFetching: false,
   data: [],
   hasError: false,
   errorMessage: null
-};
+}
 
-export default function(state = [], action) {
+export default function(state = initialState, action) {
 
   switch(action.type) {
 
     case FETCHING_COIN_DATA:
-      return Object.assign({}, state, {
+      return Object.assign(state, {
         isFetching: true,
         data: null,
         hasError: false,
         errorMessage: null
       });
 
-      case FETCHING_COIN_DATA_SUCCESS:
-        return Object.assign({}, state, {
-          isFetching: false,
-          data: action.payload,
-          hasError: false,
-          errorMessage: null
-        });
+    case FETCHING_COIN_DATA_SUCCESS:
+      return Object.assign(state, {
+        isFetching: false,
+        data: action.payload,
+        hasError: false,
+        errorMessage: null
+      });
 
-        case FETCHING_COIN_DATA_FAIL:
-          return Object.assign({}, state, {
-            isFetching: false,
-            data: action.payload,
-            hasError: true,
-            errorMessage: action.err
-          });
+    case FETCHING_COIN_DATA_FAIL:
+      return Object.assign(state, {
+        isFetching: false,
+        data: null,
+        hasError: true,
+        errorMessage: action.payload
+      });
 
-    defualt:
+
+    default:
       return state;
   }
 
